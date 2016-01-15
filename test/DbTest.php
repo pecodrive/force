c7c4b53468c4cd5679fe3de4d578a980291b0c45
pecodrive <pecodrive@gmail.co>
2016-01-15 12:42:42 +0900

    fix test

--- test/DbTest.php
+++ test/DbTest.php
@@ -9,10 +9,9 @@ class DbTest extends PHPUnit_Framework_TestCase
     }
     public function testAnalyzedPrepearSt()
     {
-        $placeHolders = [];
         $placeHolders = $this->db->analyzedPrepearSt(":peco, :anino, :desini");
 
-        var_dump($placeHolders);
+        var_dump("aaaa");
     }
 
 
